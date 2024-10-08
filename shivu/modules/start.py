9999b9ef61ab045cda8bc031c4b24d5dc9d738e4
official-ztx <wtuhin707@gmail.com>
2024-10-08 23:48:32 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -34,9 +34,13 @@ async def start(update: Update, context: CallbackContext) -> None:
         
         
         caption = f"""
-        ***Hi User...***🥰
+        ***────「 Dear | {mention} 」────***
 
-***I am An Advance Anime Character Catcher Bot..​Kidnap Me in Your group.. And I will Spawn Random Characters After.. every 100 messages in Group... Use /guess to.. Collect that Characters in Your Collection.. and see Collection by using /Harem... So add in Your groups and Collect Your harem***
+***¤ ᴡᴇ ʜᴀᴠᴇ ɴᴏ ᴡᴀʏ ᴏꜰ ꜱᴇᴇɪɴɢ ᴀʟʟ ᴛʜᴇ ᴘᴏꜱꜱɪʙɪʟɪᴛɪᴇꜱ ᴛʜᴀᴛ ʟɪᴇ ᴀʜᴇᴀᴅ ɪɴ ᴛʜᴇ ꜰᴜᴛᴜʀᴇ, ꜱᴏ ᴡᴇ ʜᴀᴠᴇ ᴛᴏ ʀᴇʟʏ ᴏɴ ᴄʟᴜᴍꜱʏ ᴛʀɪᴀʟ-ᴀɴᴅ-ᴇʀʀᴏʀ ᴛᴏ ɢᴀɪɴ ᴋɴᴏᴡʟᴇᴅɢᴇ
+⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
+🌱 VERSION : 0.0.1
+⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
+⍟ ᴄʜᴇᴄᴋ ᴏᴜᴛ ᴍʏ ꜰᴀᴄɪʟɪᴛɪᴇꜱ ʙʏ ɢɪᴠᴇɴ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ***
         """
         
         keyboard = [
@@ -90,9 +94,13 @@ async def button(update: Update, context: CallbackContext) -> None:
     elif query.data == 'back':
 
         caption = f"""
-        ***Hi User... *** ✨
+        ***Hi User... ────「 Dear | {mention} 」────*** ✨
 
-***I am An Advance Anime Character Catcher Bot..​Kidnap Me in Your group.. And I will Spawn Random Characters After.. every 100 messages in Group... Use /guess to.. Collect that Characters in Your Collection.. and see Collection by using /Harem... So add in Your groups and Collect Your harem***
+***¤ ᴡᴇ ʜᴀᴠᴇ ɴᴏ ᴡᴀʏ ᴏꜰ ꜱᴇᴇɪɴɢ ᴀʟʟ ᴛʜᴇ ᴘᴏꜱꜱɪʙɪʟɪᴛɪᴇꜱ ᴛʜᴀᴛ ʟɪᴇ ᴀʜᴇᴀᴅ ɪɴ ᴛʜᴇ ꜰᴜᴛᴜʀᴇ, ꜱᴏ ᴡᴇ ʜᴀᴠᴇ ᴛᴏ ʀᴇʟʏ ᴏɴ ᴄʟᴜᴍꜱʏ ᴛʀɪᴀʟ-ᴀɴᴅ-ᴇʀʀᴏʀ ᴛᴏ ɢᴀɪɴ ᴋɴᴏᴡʟᴇᴅɢᴇ
+⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
+🌱 VERSION : 0.0.1
+⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
+⍟ ᴄʜᴇᴄᴋ ᴏᴜᴛ ᴍʏ ꜰᴀᴄɪʟɪᴛɪᴇꜱ ʙʏ ɢɪᴠᴇɴ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ***
         """
 
         
