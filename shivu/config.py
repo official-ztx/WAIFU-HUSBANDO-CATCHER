5267e1317a9ca6e59d52f02ef83ae49f7f2d32c1
official-ztx <wtuhin707@gmail.com>
2024-10-27 12:07:11 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "7089408502"
-    sudo_users = "6845325416", "1647602447"
-    GROUP_ID = -1002210286036
-    TOKEN = "7564089592:AAH9w8KMHroJqxTFhWxJky1TKGCyVQWzxfg"
+    OWNER_ID = "7038202445"
+    sudo_users = "7757912959",
+    GROUP_ID = -1002252184024
+    TOKEN = "7297627525:AAGwV9BHee2Qisn4u12--4710r26OhHBWRk"
     mongo_url = "mongodb+srv://GOKU:MISSBHOPALI@goku.pzzsl8d.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://files.catbox.moe/ofeqts.jpg"]
-    SUPPORT_CHAT = "ANIME_CHAT_GROUP_ZTX"
-    UPDATE_CHAT = "ZTX_ORG"
-    BOT_USERNAME = "Guess_Your_Character_bot"
-    CHARA_CHANNEL_ID = "-1002210286036"
-    api_id = 28928028
-    api_hash = "b097202e877124392f4851d215fa8f3a"
+    SUPPORT_CHAT = "https://t.me/Anime_Group_India_Chat"
+    UPDATE_CHAT = "https://t.me/Shorekeeper_updates"
+    BOT_USERNAME = "HosinoXharembot"
+    CHARA_CHANNEL_ID = "-1002252184024"
+    api_id = 23028479
+    api_hash = "c1e6a93b04c0810a5c282d8d8d44ea6f"
 
     
 class Production(Config):
